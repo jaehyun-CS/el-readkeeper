135ae512aa38117b4721bf21971d41504895f5da
jaehyun-CS <lee.j.emmanuel@gmail.com>
2022-01-22 21:19:44 -0500

    Connected to firebase

--- src/index.js
+++ src/index.js
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from 'firebase/app';
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDqJrnvGxtf1j_8Vp0OiIAUYN1miWsKaRA",
+    authDomain: "readkeeper-db159.firebaseapp.com",
+    projectId: "readkeeper-db159",
+    storageBucket: "readkeeper-db159.appspot.com",
+    messagingSenderId: "50349287327",
+    appId: "1:50349287327:web:5126797cc86b8a59033faa",
+    measurementId: "G-5L46R4XS71"
+};
+
+const app = initializeApp(firebaseConfig);
+console.log("HIIIIII");
+
 
 ReactDOM.render(
   <React.StrictMode>
