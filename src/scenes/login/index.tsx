65265d27568077b66deef0f8f609482a80251435
jaehyun-CS <lee.j.emmanuel@gmail.com>
2022-01-25 12:04:15 -0500

    Styling changes

--- src/scenes/login/index.tsx
+++ src/scenes/login/index.tsx
@@ -14,14 +14,28 @@ import './index.css';
 
 const styles = {
     loginBox: {
+        background: '#EBEEF2',
         textAlign: 'center',
         margin: '5em auto 5em auto',
         width: '30%',
-        padding: '2em 0 2em 0'
+        padding: '2em 2em 2em 2em'
     },
     fieldBox: {
         margin: '1em auto 1em auto',
         display: 'block',
+    },
+    title: {
+        color: '#402312',
+        fontFamily: 'Didot',
+        paddingBottom: '.5em'
+    },
+    button: {
+        margin: '1em auto 1em auto',
+        display: 'block',
+        background: '#8C6542',
+        hover: {
+            background: '#8C6542'
+        }
     }
 };
 
@@ -34,6 +48,7 @@ const handleSubmit = (data: { email: string, password: string }, { setSubmitting
 const LoginPage = (): JSX.Element => {
     return (
         <Paper sx={ styles.loginBox }>
+            <Typography variant='h2' sx={ styles.title }>ReadKeeper</Typography>
             <Typography variant='h5'>Log in</Typography>
             <Formik
                 initialValues={{
@@ -45,6 +60,7 @@ const LoginPage = (): JSX.Element => {
                 {({ values, isSubmitting }) => (
                     <Form>
                         <Field
+                            fullWidth
                             placeholder='Email Address'
                             name='email'
                             type='input'
@@ -53,6 +69,7 @@ const LoginPage = (): JSX.Element => {
                         />
 
                         <Field
+                            fullWidth
                             placeholder='Password'
                             name='password'
                             type='input'
@@ -60,7 +77,7 @@ const LoginPage = (): JSX.Element => {
                             sx={ styles.fieldBox }
                         />
 
-                        <Button disabled={ isSubmitting } variant='contained' sx={ styles.fieldBox } type='submit'>
+                        <Button disabled={ isSubmitting } variant='contained' sx={ styles.button } type='submit'>
                             Log in
                         </Button>
                     </Form>
