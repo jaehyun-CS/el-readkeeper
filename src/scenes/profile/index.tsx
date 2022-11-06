3b92a3674ef1e675c52397fbd8e2307ccf43eb09
jaehyun-CS <lee.j.emmanuel@gmail.com>
2022-11-06 15:22:14 -0500

    Profile page stub

--- src/scenes/profile/index.tsx
+++ src/scenes/profile/index.tsx
@@ -1,8 +1,36 @@
-const ProfilePage = (): JSX.Element => {
+import { Grid, Typography } from '@mui/material';
+import AppHeader from '../../components/AppHeader';
+import { useAuth } from '../../context/AuthContext';
+import './index.css';
+import { getUserName } from '../../services/user-services';
 
+
+const styles = {
+    heading: {
+        fontFamily: 'Avenir Next Condensed',
+        color: '#260101',
+        fontWeight: 'medium',
+        paddingTop: '.5em',
+        marginLeft: '.7em'
+    },
+};
+
+const ProfilePage = (): JSX.Element => {
+    const { currentUser } = useAuth();
+
+    console.log( currentUser );
 
     return (
-        <></>
+        <>
+            <AppHeader />
+            <Grid container>
+                <Grid item>
+                    <Typography variant='h3' sx={ styles.heading }>
+                        {`Your Profile, ${ currentUser.uid }`}
+                    </Typography>
+                </Grid>
+            </Grid>
+        </>
     );
 };
 
